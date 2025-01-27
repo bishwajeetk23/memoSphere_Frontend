256c7e67cab24fdbcfd88ed0a941b4976e820ab8
Bishwajeet <bishwajeetk23@gmail.com>
2025-01-27 16:40:59 +0530

    Card component done

--- src/components/ui/Card_component.tsx
+++ src/components/ui/Card_component.tsx
@@ -1,49 +1,59 @@
-import { ReactElement } from "react";
 import { DeleteIcon } from "../../icons/deleteIcon";
 import { DocsIcon } from "../../icons/docsIcon";
 import { ShareIcon } from "../../icons/ShareIcon";
-type sizeVaraint = "sm" | "md" | "lg";
+type typeVaraint = "twitter" | "youtube";
 
 interface Cardprops{
-    size: sizeVaraint,
-    className?:string,
-    children?: ReactElement
+    title: string,
+    link: string,
+    type: typeVaraint
 }
 
-const sizeStyle = {
-    "sm": "px-2 py-2",
-    "md": "px-2 py-4",
-    "lg": "px-2 py-6"
-}
-export function Card(props:Cardprops){
+export function Card({title,link,type}:Cardprops){
+    // TODO: find cleaner way to embed the twitter and youtube link
+    // if(type === "youtube")console.log(link.replace("watch","embed").replace("?v=","/"));
+    
     return (
-        <div className={`px-4 py-4 bg-white-800 rounded-md border border-gray-200 max-w-64 min-h-80`}>
-            <div className="flex justify-between">
-                <div className="flex items-center gap-[5px] text-gray-500 text-xs">
-                    <DocsIcon size='md' />
-                    <p className="text-black">Project Ideas</p>
+        <div>
+            <div className={`px-4 py-4 bg-white-800 rounded-md border border-gray-200 max-w-72 min-h-48 min-w-72`}>
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-[5px] text-gray-500 text-xs">
+                        <DocsIcon size='md' />
+                        <p className="text-black">{title}</p>
+                    </div>
+                    <div className="flex items-center gap-[5px] text-gray-500">
+                        <a href={link} target="_blank">
+                            <ShareIcon size="md"/>
+                        </a>
+                        <DeleteIcon size="md"/>
+                    </div>
                 </div>
-                <div className="flex items-center gap-[5px] text-gray-500">
-                    <ShareIcon size="md"/>
-                    <DeleteIcon size="md"/>
+                <div className="pt-4">
+                    {
+                        type === "youtube" &&  <iframe 
+                            className={`w-full h-auto`}    
+                            src={link.replace("watch","embed").replace("?v=","/")} 
+                            title="YouTube video player" 
+                            frameBorder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            referrerPolicy="strict-origin-when-cross-origin" 
+                            allowFullScreen
+                        >
+                        </iframe> 
+                        
+                    }
+                    {
+                        type === "twitter" && <blockquote className={`w-full h-auto twitter-tweet`}  >
+                            <a href={link.replace("x.com","twitter.com")}></a>
+                        </blockquote> 
+                    }
                 </div>
             </div>
-            <div className="pt-4">
-                <iframe 
-                    className={`w-full h-full`}    
-                    src="https://www.youtube.com/embed/-frNQkRz_IU?si=yLf5e__rvfA5UGez" 
-                    title="YouTube video player" 
-                    frameBorder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                    referrerPolicy="strict-origin-when-cross-origin" 
-                    allowFullScreen
-                >
-                </iframe>
-            </div>
-            
         </div>
+        
     );
 }
-{/* <div className="flex items-center font-bold">
-                <h1>Future Projects</h1>
-</div> */}
+
+
+
+
