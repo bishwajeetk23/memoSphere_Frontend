5be47da968b9e101e8c49d765d4cf18b77f3e634
Bishwajeet <bishwajeetk23@gmail.com>
2025-01-27 14:50:01 +0530

    Added all icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css'
-import { Button } from './components/ui/Button';
 import { Card } from './components/ui/Card_component';
-import { PlusIcon } from './icons/PlusIcon';
 function App() {
 
   return (
     <>
-    <div className='h-screen bg-blue-50'>
-        <PlusIcon size='md' />
-        <Card size='md' className={'w-[300px] h-[400px]'}>
-        </Card>
+    <div className='h-screen bg-blue-500'>
+        <Card size='md' className={'w-[300px] h-[400px]'} />
     </div>
     </>
   )
--- src/components/ui/Card_component.tsx
+++ src/components/ui/Card_component.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
-import { PlusIcon } from "../../icons/PlusIcon"; 
 import { DeleteIcon } from "../../icons/deleteIcon";
+import { DocsIcon } from "../../icons/docsIcon";
+import { ShareIcon } from "../../icons/ShareIcon";
 type sizeVaraint = "sm" | "md" | "lg";
 
 interface Cardprops{
@@ -16,20 +17,33 @@ const sizeStyle = {
 }
 export function Card(props:Cardprops){
     return (
-        <div className={`px-2 py-4 bg-white-800 rounded-md border border-gray-200 max-w-64 min-h-80`}>
+        <div className={`px-4 py-4 bg-white-800 rounded-md border border-gray-200 max-w-64 min-h-80`}>
             <div className="flex justify-between">
                 <div className="flex items-center gap-[5px] text-gray-500 text-xs">
-                    <PlusIcon size='md' />
+                    <DocsIcon size='md' />
                     <p className="text-black">Project Ideas</p>
                 </div>
                 <div className="flex items-center gap-[5px] text-gray-500">
+                    <ShareIcon size="md"/>
                     <DeleteIcon size="md"/>
-                    <PlusIcon size="md"/>
                 </div>
             </div>
-            <div className="flex items-center font-bold">
-                <h1>Future Projects</h1>
+            <div className="pt-4">
+                <iframe 
+                    className={`w-full h-full`}    
+                    src="https://www.youtube.com/embed/-frNQkRz_IU?si=yLf5e__rvfA5UGez" 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerPolicy="strict-origin-when-cross-origin" 
+                    allowFullScreen
+                >
+                </iframe>
             </div>
+            
         </div>
     );
 }
+{/* <div className="flex items-center font-bold">
+                <h1>Future Projects</h1>
+</div> */}
